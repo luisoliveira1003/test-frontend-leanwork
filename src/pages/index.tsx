3aeb649f589109b52f20ebbaa154e77d4960f0c0
Luis André <luis.oliveira10031992@gmail.com>
2021-08-06 00:06:30 -0300

    Challenge Frontend LeanWork completed :rocket:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import AddForm from "../components/People/AddForm";
 import PeopleContextProvider from "../contexts/PeopleContext";
-import { Container, Content } from "./Home.style";
+import { Container, Content } from "../../styles/Home.style";
 
 export default function Home() {
   return (
